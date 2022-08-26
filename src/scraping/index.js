97b01890129563c70c52b7daaa5048b581702576
Braian Molina <braian_gabriel_1993@hotmail.com>
2022-08-25 22:16:00 -0300

    se agrega pagina de inmobiliaria

--- src/scraping/index.js
+++ src/scraping/index.js
@@ -1,13 +1,41 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const puppeteer = require('puppeteer');
 
+// ---------
+// variables
+// ---------
+const DOMAIN = 'https://www.zonaprop.com.ar/';
+const UBICATION = 'villa-urquiza-capital-federal';
+const URL = `${DOMAIN}departamentos-venta-q-${UBICATION}.html`;
+const PATH_FILE_IMAGE = 'src/scraping/images/';
+const INPUT_SEARCH = '[data-qa=search-input] input';
+//const BUTTON_SEARCH = '[data-qa=search-button]';
+const BUTTON_SEARCH = '.apply-filters button';
+const COMPONENT_CARD = 'postings-container';
+
 (async () => {
   //headless: open browser  [temp]
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.es');
-  await page.screenshot({ path: 'src/scraping/images/example.png' });
 
-  //await page.waitForTimeout(5000);
+  await page.goto(URL);
+  // capture screenshot
+  await page.screenshot({ path: PATH_FILE_IMAGE + 'home.png' });
+
+  // write in input tag
+  //await page.type(INPUT_SEARCH, 'Villa Urquiza, Capital Federal');
+  //await new Promise((r) => setTimeout(r, 3000));
+  // capture screenshot
+  //await page.screenshot({ path: PATH_FILE_IMAGE + 'inputTag.png' });
+
+  // click on button search
+  //await page.click(BUTTON_SEARCH);
+  //await for appear this selector
+  //await page.waitForSelector(COMPONENT_CARD);
+  // capture screenshot
+  //await page.screenshot({ path: PATH_FILE_IMAGE + 'villa_urquiza.png' });
+
+  //await for specific time for watch the result on navegator
+  await new Promise((r) => setTimeout(r, 5000));
   await browser.close();
 })();
