b0683f144e8b4a2dffb730763f6e7a698545139b
Braian Molina <braian_gabriel_1993@hotmail.com>
2022-08-28 17:34:44 -0300

    se obtiene direccion de publicaicon

--- src/scraping/index.ts
+++ src/scraping/index.ts
@@ -1,23 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const puppeteer = require('puppeteer');
-import { PARAMS_SEARCHER, QUERY_SEARCHER } from './models/page.model';
+import {
+  PARAMS_SEARCHER,
+  QUERY_SEARCHER,
+  urlParser,
+} from './models/page.model';
 
 // ---------
 // variables
 // ---------
 const URL_PAGE = `${PARAMS_SEARCHER.DOMAIN}${PARAMS_SEARCHER.DEPARTAMENTOS}-${PARAMS_SEARCHER.ALQUILER}-${PARAMS_SEARCHER.UBICATION}.html`;
 const PATH_FILE_IMAGE = 'src/scraping/images/';
+const settingLunch = {
+  headless: false,
+  slowMo: 0,
+  devtools: false,
+  defaultViewport: null,
+};
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 0,
-    devtools: false,
-    defaultViewport: null,
-  });
+  const browser = await puppeteer.launch(settingLunch);
   const page = await browser.newPage();
 
-  await page.goto(URL_PAGE, {waitUntil: 'load'});
+  await page.goto(URL_PAGE, { waitUntil: 'load' });
   // capture screenshot
   await page.screenshot({ path: PATH_FILE_IMAGE + 'home.png' });
 
@@ -28,34 +33,88 @@ const PATH_FILE_IMAGE = 'src/scraping/images/';
   // output: 193
   const totalResult = await page.$eval(
     QUERY_SEARCHER.TOTAL_RESULT,
-    (document) => Number(document.textContent.split(' ', 1)[0]),
+    (selector) => Number(selector.textContent.split(' ', 1)[0]),
   );
 
   // average page by total result cosidering 20 page per page
-  const pageCountAvg = Math.round(totalResult/20);
-  console.log("totalResult: ", totalResult) //[x]
-  console.log("pageCountAvg: ", pageCountAvg) //[x]
+  const pageCountAvg = Math.round(
+    totalResult / PARAMS_SEARCHER.QTY_RESULT_DEFAULT,
+  );
+  console.log('totalResult: ', totalResult); //[x]
+  console.log('pageCountAvg: ', pageCountAvg); //[x]
 
   // $$eval = querySelectorAll
   // extract all url pages per card
-  let arrCardlinks = await page.$$eval(
-    QUERY_SEARCHER.CARD_URL,
-    (document) => {
-      const links = [];
-      document.forEach(item => {
-        links.push(item.attributes.getNamedItem("data-to-posting").textContent)
-      });
-      return links;
-    }
-  );
+  let arrCardlinks = await page.$$eval(QUERY_SEARCHER.CARD_URL, (selector) => {
+    const links = [];
+    selector.forEach((item) => {
+      links.push(item.attributes.getNamedItem('data-to-posting').textContent);
+    });
+    return links;
+  });
+  await browser.close();
 
-  // add domain for each endPoint page
+  // add domain for each endPoint link
   // item: remove first slash from endPoint
-  arrCardlinks = arrCardlinks.map(item => PARAMS_SEARCHER.DOMAIN + item.substring(1))
+  arrCardlinks = arrCardlinks.map(
+    (item) => PARAMS_SEARCHER.DOMAIN + item.substring(1),
+  );
+  console.log('arrCardlinks: ', arrCardlinks); //[x]
 
-  console.log("arrCardlinks: ", arrCardlinks) //[x]
+  // access to each link
+  // open a new browser if required for security of page
+  for (const link of arrCardlinks) {
+    const browserDet = await puppeteer.launch(settingLunch);
+    const pageDet = await browserDet.newPage();
+
+    await pageDet.goto(link, { waitUntil: 'load' });
+
+    //get price
+    await pageDet.waitForSelector(QUERY_SEARCHER.PRICE);
+    const price = await pageDet.$eval(
+      QUERY_SEARCHER.PRICE,
+      (selector) => selector?.innerText,
+    );
+    console.log('price: ', price); //[x]
+
+    // get expense
+    // if element not exist, add field with null data
+    let expense: string;
+    try {
+      await pageDet.waitForSelector(QUERY_SEARCHER.EXPENSE);
+      expense = await pageDet.$eval(
+        QUERY_SEARCHER.EXPENSE,
+        (selector) => selector?.innerText,
+      );
+    } catch (error) {
+      expense = null;
+    }
+    console.log('expense: ', expense); //[x]
+
+    //get date published and views
+    await pageDet.waitForSelector(QUERY_SEARCHER.PUB_VIEW);
+    const published = await pageDet.$$eval(
+      QUERY_SEARCHER.PUB_VIEW,
+      (selector) => selector[0]?.textContent,
+    );
+    const views = await pageDet.$$eval(
+      QUERY_SEARCHER.PUB_VIEW,
+      (selector) => selector[1]?.textContent,
+    );
+    console.log('published: ', published); //[x]
+    console.log('views: ', views); //[x]
+
+    //get address
+    await pageDet.waitForSelector(QUERY_SEARCHER.ADDRESS);
+    const address = await pageDet.$eval(
+      QUERY_SEARCHER.ADDRESS,
+      (selector) => selector.innerText.split('\n')[0],
+    );
+    console.log('address: ', address); //[x]
+
+    await browserDet.close();
+  }
 
   //await for specific time for watch the result on navegator
   //await new Promise((r) => setTimeout(r, 60000));
-  await browser.close();
 })();
--- src/scraping/models/page.model.ts
+++ src/scraping/models/page.model.ts
@@ -1,5 +1,6 @@
 enum PARAMS_SEARCHER {
   DOMAIN = 'https://www.zonaprop.com.ar/',
+  QTY_RESULT_DEFAULT = 20,
   UBICATION = 'villa-urquiza',
   ALQUILER = 'alquiler',
   VENTA = 'venta',
@@ -10,6 +11,107 @@ enum PARAMS_SEARCHER {
 enum QUERY_SEARCHER {
   TOTAL_RESULT = '[class^=postingsTitle__Title]',
   CARD_URL = '[class^=components__CardContainer] [data-to-posting]',
+  PRICE = '.price-items span',
+  EXPENSE = '[class^=block-expensas] span',
+  PUB_VIEW = '#user-views p',
+  ADDRESS = '.title-location',
+  FEATURE_DEPT = '.section-icon-features .icon-feature',
+  FEATURE_GRAL = '#reactGeneralFeatures [data-element=accordionContent] li',
+  GEOLOCATION = '#article-map #static-map',
 }
 
-export { PARAMS_SEARCHER, QUERY_SEARCHER };
+enum KeyValue {
+  KEY = 'key',
+  VALUE = 'value',
+}
+
+enum UrlQuery {
+  CENTER = 'center',
+  ZOOM = 'zoom',
+  MARKERS = 'markers',
+  KEY = 'key',
+  SIZE = 'size',
+  SENSOR = 'sensor',
+  SCLAE = 'scale',
+  SIGNATURE = 'signature',
+  CHANNEL = 'channel',
+}
+interface IGeolocation {
+  position?: string;
+  latitude?: string;
+  longitude?: string;
+}
+
+interface IUrlQuery {
+  center?: string;
+  zoom?: string;
+  markers?: string;
+  geolocation?: IGeolocation;
+  key?: string;
+  size?: string;
+  sensor?: string;
+  scale?: string;
+  signature?: string;
+  channel?: string;
+}
+
+const getKeyValue = (data: string, output: string): string => {
+  // separate data for extract key/value
+  const arrData = data.split('=');
+
+  switch (output) {
+    case KeyValue.KEY:
+      return arrData[0].toString();
+    case KeyValue.VALUE:
+      return arrData[1].toString();
+    default:
+      return '';
+  }
+};
+
+const urlParser = (data: string) => {
+  //const obj ={}
+  const obj: IUrlQuery = {};
+  // get query params
+  data = data.substring(data.indexOf('?') + 1);
+  // remove text for concatenate
+  data = data.replaceAll('&amp', '');
+  // separate data per ";" simbol
+  const arrData = data.split(';');
+
+  // save data with specific fields
+  arrData.forEach((item) => {
+    switch (getKeyValue(item, KeyValue.KEY)) {
+      case UrlQuery.CENTER:
+        obj.center = getKeyValue(item, KeyValue.VALUE);
+      case UrlQuery.MARKERS:
+        obj.markers = getKeyValue(item, KeyValue.VALUE);
+      case UrlQuery.SIZE:
+        obj.size = getKeyValue(item, KeyValue.VALUE);
+    }
+  });
+
+  // format geolocation data
+  // only if exist data on "center" or "markers"  fields
+  if (obj.markers) {
+    obj.geolocation = {
+      position: obj.markers,
+      latitude: obj.markers.split(',')[0],
+      longitude: obj.markers.split(',')[1],
+    };
+  }
+  return obj;
+};
+
+interface IDataEstate {
+  price?: string;
+  expense?: string;
+  published?: string;
+  views?: string;
+  address?: string;
+  featureDept?: string[];
+  featureGral?: string[];
+  geolocation?: IGeolocation;
+}
+
+export { PARAMS_SEARCHER, QUERY_SEARCHER, urlParser, IDataEstate };
