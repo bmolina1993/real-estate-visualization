c06fad789b269d0b0141022ac808b9a6ba625bba
Braian Molina <braian_gabriel_1993@hotmail.com>
2022-09-02 21:59:34 -0300

    se conecta postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Client } from 'pg';
+
+// instancia configuracion postgresql
+const client = new Client({
+  user: 'admin',
+  host: '192.168.149.128',
+  database: 'mydb',
+  password: 'admin',
+  port: 5432,
+});
+
+(async () => {
+  //conecta
+  await client.connect();
+  try {
+    //envia consulta
+    const response = await client.query('select * from tasks;');
+    console.log('response.rows: ', response.rows);
+  } catch (error) {
+    console.log('error.stack: ', error.stack);
+  }
+  //des-conecta
+  await client.end();
+})();
 
 @Module({
   imports: [],
